ad9017abbdf41af7882d592f62c679dd41e99b88
DanLockhart <DanLockhart@users.noreply.github.com>
2020-04-23 14:40:19 -0700

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,12 @@
 def main():
+  print("Keep it logically awesome.")
+
+
+if __name__=="__main__":
+  main()
+
+
+# def main():
   # print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -7,5 +15,5 @@ def main():
 
   #print(quotes)
 
-if __name__== "__main__":
-  main()
+# if __name__== "__main__":
+  # main()
